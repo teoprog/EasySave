135572feaccdaa8e1b48ca7a72f72fb5ce094573
AymaneHilmi <100952544+AymaneHilmi@users.noreply.github.com>
2023-02-03 11:19:50 +0100

    Affichage console + architecture sauvegarde

--- Program.cs
+++ Program.cs
@@ -1,15 +1,34 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System;
+using BackupApplication;
 
-namespace EasySave
+namespace ConsoleBackupApp
 {
-    internal class Program
+    class Program
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Formulaire de sauvegarde :");
+            Console.Write("Appellation : ");
+            string appellation = Console.ReadLine();
+            Console.Write("fichier Source : ");
+            string sourceDirectory = Console.ReadLine();
+            Console.Write("Fichier cible : ");
+            string targetDirectory = Console.ReadLine();
+            Console.Write("Type de Sauvegarde (Complete or différentielle) : ");
+            string type = Console.ReadLine();
+
+            BackupJob job = new BackupJob
+            {
+                Appellation = appellation,
+                SourceDirectory = sourceDirectory,
+                TargetDirectory = targetDirectory,
+
+                // Type = type == "Complete" ? BackupJob.BackupType.Complete : BackupJob.BackupType.Differential
+            };
+
+            job.Save();
+
+            Console.WriteLine("La sauvegarde de type " + type + "du fichier " + appellation + " a bien été réalisée avec succès");
         }
     }
 }
