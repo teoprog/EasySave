23e43936651091671ffe37ded31ce9cc9274bd29
teoprog <emirot.teo@gmail.com>
2023-02-16 15:16:42 +0100

    encrypt working proprely

---
+++ CryptoSoft/Main.cs
@@ -0,0 +1,47 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace EasySave
+{
+    internal static class MainPage
+    {
+        private static int Main(string[] args)
+        {
+            try
+            {
+                string inputFile = args[0];
+                long fSize = (new FileInfo(inputFile)).Length;
+
+                string password = args[1];
+                byte[] salt = new byte[] { 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08 };
+
+                byte[] bytesToEncrypt = Encoding.UTF8.GetBytes(File.ReadAllText(inputFile));
+
+                using (Aes aes = Aes.Create())
+                {
+                    aes.KeySize = 256;
+                    aes.BlockSize = 128;
+                    var key = new Rfc2898DeriveBytes(password, salt, 1000).GetBytes(aes.KeySize / 8);
+                    aes.Key = key;
+                    aes.IV = new byte[aes.BlockSize / 8];
+
+                    using (FileStream output = new FileStream(inputFile, FileMode.Create))
+                    using (CryptoStream encryptor = new CryptoStream(output, args[2] != "decrypt" ? aes.CreateEncryptor() : aes.CreateDecryptor(), CryptoStreamMode.Write))
+                    {
+                        encryptor.Write(bytesToEncrypt, 0, bytesToEncrypt.Length);
+                    }
+                }
+                new FileInfo(inputFile);
+                if (fSize == (new FileInfo(inputFile)).Length)
+                {
+                    return -1;
+                }
+
+            } catch (Exception ex)
+            {
+                return -1;
+            }
+            return 0;
+        }
+    }
+}
---
+++ EasySave/GeneralTools.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace EasySave
+{
+    public static class GeneralTools
+    {
+        /// <summary>
+        /// Var who stock the path to the logs directory
+        /// </summary>
+        public static readonly string? LogPath = new ConfigurationBuilder()
+            .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true).Build()["Log_Path"];
+        
+        /// <summary>
+        /// A simple function who write a warning message
+        /// </summary>
+        /// <param name="word">Message you want to write on Console</param>
+        internal static void WriteWarningMessage(string word) {
+            ConsoleColor temp = Console.ForegroundColor;
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+            Console.WriteLine(word);
+            Console.ForegroundColor = temp;
+        }
+        
+        /// <summary>
+        /// Create log folder and logs files if they don't exist
+        /// </summary>
+        public static void CreateLogsFiles()
+        {
+            if (!Directory.Exists(LogPath))
+                Directory.CreateDirectory(LogPath);
+
+            if(!File.Exists(LogPath + "/logs.json"))
+                File.Create(LogPath + "/logs.json");
+                
+            
+            if(!File.Exists(LogPath + "/state.json"))
+                File.Create(LogPath + "/state.json");
+        }
+        
+        /// <summary>
+        /// Get the valid directory of a path :
+        ///  ex : C:/Bureau/rep --> get C:/Bureau cause rep does not exist
+        /// </summary>
+        /// <param name="path">Invalid path</param>
+        /// <returns>The valid path</returns>
+        public static string GetValidDirectoryPath(string? path)
+        {
+            if (path != null && !path.Contains(@"\") && !path.Contains("/")) return "";
+            while (!Directory.Exists(path)) path = Path.GetDirectoryName(path);
+            return path;
+        }
+    }
+}
