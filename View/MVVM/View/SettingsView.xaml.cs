40f97b993ad3aff14a6dcc299215f1c3d46314a6
AymaneHilmi <aymanehilmi1@gmail.com>
2023-02-20 19:08:08 +0100

    maj Homeview + button source update

--- View/MVVM/View/SettingsView.xaml.cs
+++ View/MVVM/View/SettingsView.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using View;
 
 namespace View.MVVM.View
 {
@@ -24,7 +25,63 @@ namespace View.MVVM.View
         {
             InitializeComponent();
         }
+        
+        
 
-       
+        private void ChekBox1_Click(object sender, RoutedEventArgs e)
+        {
+
+            if (MainWindow.option == false)
+            {
+                /*HomeButton.Text = "Accueil";
+                HomeCompleteBackupButton.Text = "Sauvegarde complete";
+                HomeDifferentialBackupButton.Text = "Sauvegarde Sequentielle";
+                HomeBlackListSoftwareButton.Text = "Logiciels Metier";
+                HomeSettingsButton.Text = "Parametres";
+                HomeAboutUsButton.Text = "A propos";
+                HomeDashBoardCompleteSaves.Text = "Sauvegarde Complete";
+                HomeDashBoardDifferentialSaves.Text = "Sauvegarde Sequentielle";
+                HomeTableTitleName.Text = "Nom";
+                HomeTableTitleSource.Text = "Source";
+                HomeTableTitleTarget.Text = "Destination";
+                Homejobsbutton.Text = "Envoyer";
+                SaveTitleName.Text = "Nom de la Sauvegarde";
+                SaveTitleSource.Text = "Repertoire Source";
+                SaveTitleTarget.Text = "Repertoire de Destination";
+                SaveSendButton.Text = "Envoyer";*/
+                Settingstitle.Text = "Parametres";
+                SettingsLanguage.Text = "Langue";
+                SettingsLogFiles.Text = "Extension des fichier Logs";
+                SettingsEncryptingTitle.Text = "Chiffrement";
+                SettingsSizeSave.Text = "Taille Maximale d'une Save";
+
+                MainWindow.option = true;
+            }
+            else if (MainWindow.option == true)
+            {
+               /*HomeButton = "Home";
+               HomeCompleteBackupButton = "Complete Saves";
+               HomeDifferentialBackupButton = "Differential Saves";
+               HomeBlackListSoftwareButton = "BlackList Software";
+               HomeSettingsButton = "Settings";
+               HomeAboutUsButton = "About Us";
+               HomeDashBoardCompleteSaves = "Complete Saves";
+               HomeDashBoardDifferentialSaves = "Differential Saves";
+               HomeTableTitleName = "Name";
+               HomeTableTitleTarget = "Target";
+               Homejobsbutton = "Send";
+               SaveTitleName = "BackUp Name";
+               SaveTitleSource = "Source Path";
+               SaveTitleTarget = "Target Path";
+               SaveSendButton = "Send";*/
+               Settingstitle.Text = "Settings";
+               SettingsLanguage.Text = "Language";
+               SettingsLogFiles.Text = "Extension of Logs files";
+               SettingsEncryptingTitle.Text = "Encrypting Settings";
+               SettingsSizeSave.Text = "Size Max of a Save";
+
+                MainWindow.option = false;
+            }
+        }
     }
 }
--- View/MainWindow.xaml.cs
+++ View/MainWindow.xaml.cs
@@ -1,17 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using View.MVVM.View;
+using View;
 
 namespace View
 {
@@ -20,6 +11,13 @@ namespace View
     /// </summary>
     public partial class MainWindow : Window
     {
+        public static bool option = false;
+
+
+
+
+
+
 
         public MainWindow()
         {
@@ -50,4 +48,8 @@ namespace View
 
         }
     }
+
+
+
+
 }
